2a613826bcdda2830bd5f05de8825b2d9d9ec860
adultmalehuman <adultmalehuman@gmail.com>
2020-01-28 09:20:00 -0400

    Built-in datatypes in Kotlin

---
+++ projects/Datatypes/src/com/jimtough/java/datatypes/DummyClass.java
@@ -0,0 +1,9 @@
+package com.jimtough.java.datatypes;
+
+public class DummyClass {
+
+    public String isWhatever(boolean whatever) {
+        return whatever ? "Yeah, whatever." : "No way!";
+    }
+
+}
---
+++ projects/Datatypes/src/com/jimtough/kotlin/datatypes/Datatypes.kt
@@ -0,0 +1,34 @@
+package com.jimtough.kotlin.datatypes
+
+import com.jimtough.java.datatypes.DummyClass
+
+fun main(args: Array<String>) {
+    val myInt = 10
+    var myLong = 22L
+    // Error! No automatic widening of number type variables in Kotlin, unlike Java.
+    //myLong = myInt
+    myLong = myInt.toLong()
+
+    val myByte: Byte = 111
+    var myShort: Short
+    myShort = myByte.toShort()
+
+    // Default floating point type is Double, like in Java
+    var myDouble = 65.984
+    println(myDouble is Double)
+
+    val myFloat = 838.8492f
+    println("Is 'myFloat' a Float? ${myFloat is Float}")
+
+    val myChar1 = 'b'
+    val int65 = 65
+    val myChar2: Char = int65.toChar()
+    println(myChar1 + " " + myChar2)
+
+    val myBoolean = true
+
+    val whatever = true
+    val whateverResult = DummyClass().isWhatever(whatever)
+    println(whateverResult)
+    
+}
